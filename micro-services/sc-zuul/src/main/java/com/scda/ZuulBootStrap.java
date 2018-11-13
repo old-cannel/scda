2c050aadd45c658d3e592e79c69f1c7ad2a7d84d
sunnyliuqi <liu963277983>
2018-11-13 13:33:31 +0800

    追加网关模块

--- micro-services/sc-zuul/src/main/java/com/scda/ZuulBootStrap.java
+++ micro-services/sc-zuul/src/main/java/com/scda/ZuulBootStrap.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * @Auther: liuqi
@@ -25,6 +26,7 @@ public class ZuulBootStrap {
     @Bean
     @RefreshScope
     @ConfigurationProperties("zuul")
+    @Primary
     public ZuulProperties zuulProperties() {
         return new ZuulProperties();
     }
